19420d3f0533744b5bc44f0c8250c5bdbcddd2c1
Sephix <Sephix57@gmail.com>
2019-01-15 15:10:12 +0100

    Update parsed jSON

--- meteo.js
+++ meteo.js
@@ -1,12 +1,12 @@
-const xhr = new XMLHttpRequest();
+var xhr = new XMLHttpRequest();
 
 // Forme générale du lien :
 // http://maps.openweathermap.org/maps/2.0/weather/TA2/{z}/{x}/{y}?
 // date=1527811200&opacity=0.9&fill_bound=true&appid={api_key}
 
-const base_url = "http://api.openweathermap.org/data/2.5/weather";
-const city = "Metz";
-const appid = "3c084bd74c2f77f02d6d6c30c2018bf0";
+var base_url = "http://api.openweathermap.org/data/2.5/weather";
+var city = "Metz";
+var appid = "3c084bd74c2f77f02d6d6c30c2018bf0";
 
 function get_url() {
     return base_url + "?"
@@ -16,8 +16,14 @@ function get_url() {
 
 function init_page() {
     xhr.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-            document.getElementById("meteo").innerHTML = this.responseText;
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this);
+            let weatherJson;
+            weatherJson = this.responseText;
+            console.log(weatherJson);
+            let parsed = JSON.parse(weatherJson);
+            console.log(parsed);
+            document.getElementById("meteo").innerHTML = "The weather is " + parsed.weather[0].main + ", temperature is " + (Math.round(parsed.main.temp - 273.15)) + " degree celsius" + ", wind speed is " + parsed.wind.speed + "" ;
         }
     };
 
