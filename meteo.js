7fa3934fcab30e5f865de0ccd22553e06f1e6978
Sephix <Sephix57@gmail.com>
2019-01-15 15:45:31 +0100

    appid

--- meteo.js
+++ meteo.js
@@ -1,12 +1,12 @@
-var xhr = new XMLHttpRequest();
+const xhr = new XMLHttpRequest();
 
 // Forme générale du lien :
 // http://maps.openweathermap.org/maps/2.0/weather/TA2/{z}/{x}/{y}?
 // date=1527811200&opacity=0.9&fill_bound=true&appid={api_key}
 
-var base_url = "http://api.openweathermap.org/data/2.5/weather";
-var city = "Metz";
-var appid = "3c084bd74c2f77f02d6d6c30c2018bf0";
+const base_url = "http://api.openweathermap.org/data/2.5/weather";
+const city = "Metz";
+const appid = "f5e810531af1756846022c6f387acf25";
 
 function get_url() {
     return base_url + "?"
@@ -16,10 +16,9 @@ function get_url() {
 
 function init_page() {
     xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
             console.log(this);
-            let weatherJson;
-            weatherJson = this.responseText;
+            let weatherJson = this.responseText;
             console.log(weatherJson);
             let parsed = JSON.parse(weatherJson);
             console.log(parsed);
