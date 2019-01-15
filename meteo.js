e195510472f745ab43b32c3736b719f3690cfc19
Sephix <Sephix57@gmail.com>
2019-01-15 16:23:26 +0100

    units

--- meteo.js
+++ meteo.js
@@ -7,11 +7,13 @@ const xhr = new XMLHttpRequest();
 const base_url = "http://api.openweathermap.org/data/2.5/weather";
 const city = "Metz";
 const appid = "f5e810531af1756846022c6f387acf25";
+const unit = "metric";
 
 function get_url() {
     return base_url + "?"
         + "q=" + city + "&"
-        + "appid=" + appid;
+        + "appid=" + appid
+        + "&units=" + unit;
 }
 
 function init_page() {
@@ -21,8 +23,8 @@ function init_page() {
             console.log(weatherJson);
             let parsed = JSON.parse(weatherJson);
             console.log(parsed);
-            document.getElementById("meteo").innerHTML = "The weather is " + parsed.weather[0].main + ", it is " + (Math.round(parsed.main.temp - 273.15)) +
-            " degree celsius" + " in " + parsed.name + "";
+            document.getElementById("meteo").innerHTML = "The weather is " + parsed.weather[0].main + ", it is " + parsed.main.temp +
+            " degree celsius" + " in " + parsed.name;
             document.getElementById("url").href = get_url();
         }
     };
