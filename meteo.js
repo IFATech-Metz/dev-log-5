658dadcfc1a32796a088f047970ec74db7d5a0ce
BBanshy <belcastromathias15@gmail.com>
2019-01-15 16:38:40 +0100

    Coorect nizar faux

--- meteo.js
+++ meteo.js
@@ -5,7 +5,7 @@ const xhr = new XMLHttpRequest();
 // date=1527811200&opacity=0.9&fill_bound=true&appid={api_key}
 
 const base_url = "http://api.openweathermap.org/data/2.5/weather";
-const city = "Metz";
+let city;
 const appid = "f5e810531af1756846022c6f387acf25";
 const unit = "metric";
 
@@ -32,3 +32,21 @@ function init_page() {
     xhr.open("GET", get_url(), true);
     xhr.send()
 }
+function get_temperature() {
+    city = document.getElementById("ville").value;
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            let weatherJson = this.responseText;
+            console.log(weatherJson);
+            let parsed = JSON.parse(weatherJson);
+            console.log(parsed);
+            document.getElementById("meteo").innerHTML = "The weather is " + parsed.weather[0].main + ", it is " + parsed.main.temp +
+                " degree celsius" + " in " + parsed.name;
+            document.getElementById("url").href = get_url();
+        }
+    };
+
+    xhr.open("GET", get_url(), true);
+    xhr.send()
+}
+
